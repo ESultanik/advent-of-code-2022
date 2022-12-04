15c4a3625f7a8d754f88555d8f232379f9e4f595
Evan Sultanik <evan@sultanik.com>
2022-12-03 21:12:27 -0500

    Day 3 part 1

--- aoc2022/rucksacks.py
+++ aoc2022/rucksacks.py
@@ -1,4 +1,4 @@
-from typing import FrozenSet, Iterable, Iterator
+from typing import FrozenSet, Iterable, Iterator, Tuple
 
 from . import challenge, Path
 
@@ -82,3 +82,53 @@ def load_rucksacks(path: Path) -> Iterator[Rucksack]:
 @challenge(day=3)
 def rucksack_reorganiztion(path: Path) -> int:
     return sum(next(iter(r.compartment1 & r.compartment2)).priority for r in load_rucksacks(path))
+
+
+"""
+--- Part Two ---
+As you finish identifying the misplaced items, the Elves come to you with another issue.
+
+For safety, the Elves are divided into groups of three. Every Elf carries a badge that identifies their group. For efficiency, within each group of three Elves, the badge is the only item type carried by all three Elves. That is, if a group's badge is item type B, then all three Elves will have item type B somewhere in their rucksack, and at most two of the Elves will be carrying any other item type.
+
+The problem is that someone forgot to put this year's updated authenticity sticker on the badges. All of the badges need to be pulled out of the rucksacks so the new authenticity stickers can be attached.
+
+Additionally, nobody wrote down which item type corresponds to each group's badges. The only way to tell which item type is the right one is by finding the one item type that is common between all three Elves in each group.
+
+Every set of three lines in your list corresponds to a single group, but each group can have a different badge item type. So, in the above example, the first group's rucksacks are the first three lines:
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+And the second group's rucksacks are the next three lines:
+
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+In the first group, the only item type that appears in all three rucksacks is lowercase r; this must be their badges. In the second group, their badge item type must be Z.
+
+Priorities for these items must still be found to organize the sticker attachment efforts: here, they are 18 (r) for the first group and 52 (Z) for the second group. The sum of these is 70.
+
+Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+"""
+
+
+def load_groups(path: Path) -> Iterator[Tuple[Rucksack, Rucksack, Rucksack]]:
+    with open(path, "r") as f:
+        line_group = []
+        for line in f:
+            line_group.append(line.strip())
+            if len(line_group) == 3:
+                yield map(Rucksack.load, line_group)
+                line_group = []
+        assert not line_group
+
+
+@challenge(day=3)
+def elf_groups(path: Path) -> int:
+    total = 0
+    for r1, r2, r3 in load_groups(path):
+        common_value = (r1.compartment1 | r1.compartment2) & (r2.compartment1 | r2.compartment2) \
+                       & (r3.compartment1 | r3.compartment2)
+        assert len(common_value) == 1
+        total += next(iter(common_value)).priority
+    return total
