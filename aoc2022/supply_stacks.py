0537da5c84f2c152d18fd7c1be972d443ac805ee
Evan Sultanik <evan@sultanik.com>
2022-12-05 08:26:13 -0500

    Day 5 part 1

--- aoc2022/supply_stacks.py
+++ aoc2022/supply_stacks.py
@@ -77,16 +77,22 @@ class Move:
             raise ValueError(line)
         return cls(from_crate=int(m.group(2)) - 1, to_crate=int(m.group(3)) - 1, quantity=int(m.group(1)))
 
-    def apply(self, stacks: List[Stack]) -> List[Stack]:
+    def apply(self, stacks: List[Stack], retain_order: bool = False) -> List[Stack]:
         result = list(stacks)
         result[self.from_crate] = Stack(result[self.from_crate])
         result[self.to_crate] = Stack(result[self.to_crate])
         if len(stacks[self.from_crate]) < self.quantity:
             raise ValueError(f"Insufficient crates in stack {self.from_crate} when applying {self!s}; "
                              f"stack only has {len(stacks[self.from_crate])} crates")
-        for _ in range(self.quantity):
-            to_move = result[self.from_crate].pop()
-            result[self.to_crate].append(to_move)
+        if retain_order:
+            # Crate Mover 9001 behavior from Part 2:
+            result[self.to_crate] += result[self.from_crate][-self.quantity:]
+            result[self.from_crate] = result[self.from_crate][:-self.quantity]
+        else:
+            # Crate Mover 9000 behavior from Part 1:
+            for _ in range(self.quantity):
+                to_move = result[self.from_crate].pop()
+                result[self.to_crate].append(to_move)
         return result
 
     def __str__(self):
@@ -124,4 +130,59 @@ def top_stack(path: Path) -> str:
     stacks, moves = load(path)
     for move in moves:
         stacks = move.apply(stacks)
+    return "".join(stack[-1] for stack in stacks)
+
+
+"""
+--- Part Two ---
+As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+Again considering the example above, the crates begin in the same configuration:
+
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+[D]        
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+        [D]
+        [N]
+    [C] [Z]
+    [M] [P]
+ 1   2   3
+Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+        [D]
+        [N]
+[C]     [Z]
+[M]     [P]
+ 1   2   3
+Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+        [D]
+        [N]
+        [Z]
+[M] [C] [P]
+ 1   2   3
+In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
+"""
+
+
+@challenge(day=5)
+def rearrangement(path: Path) -> str:
+    stacks, moves = load(path)
+    for move in moves:
+        stacks = move.apply(stacks, retain_order=True)
     return "".join(stack[-1] for stack in stacks)
