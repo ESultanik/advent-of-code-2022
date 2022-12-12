3a4e9f127f03e53328ca0aa7182b4530c9ef1a2a
Evan Sultanik <evan@sultanik.com>
2022-12-12 07:58:05 -0500

    Starting the second part

--- aoc2022/monkey_in_the_middle.py
+++ aoc2022/monkey_in_the_middle.py
@@ -249,6 +249,7 @@ class Monkey:
         self.if_true: int = if_true
         self.if_false: int = if_false
         self.num_items_inspected: int = 0
+        self.worry_divisor: int = 3
 
     def take_turn(self, monkeys: Dict[int, "Monkey"]):
         for worry_level in self.items:
@@ -256,7 +257,7 @@ class Monkey:
             operand = self.operand
             if operand is None:
                 operand = worry_level
-            new_level = self.operator.function(worry_level, operand) // 3
+            new_level = self.operator.function(worry_level, operand) // self.worry_divisor
             if new_level % self.divisible_by == 0:
                 new_monkey = self.if_true
             else:
@@ -327,8 +328,7 @@ class Monkey:
 """
 
 
-@challenge(day=11)
-def monkey_business(path: Path) -> int:
+def load(path: Path) -> Dict[int, Monkey]:
     monkeys: Dict[int, Monkey] = {}
     with open(path, "r") as f:
         while True:
@@ -337,8 +337,113 @@ def monkey_business(path: Path) -> int:
                 monkeys[monkey.number] = monkey
             except ValueError as e:
                 break
+    return monkeys
+
+
+@challenge(day=11)
+def monkey_business(path: Path) -> int:
+    monkeys = load(path)
     for _ in trange(20, desc="simulating", unit="rounds", leave=False):
         for i in trange(len(monkeys), desc="running round", unit="monkeys", leave=False):
+            monkeys[i].take_turn(monkeys)
+    top_two = heapq.nlargest(2, monkeys.values(), key=lambda m: m.num_items_inspected)
+    return top_two[0].num_items_inspected * top_two[1].num_items_inspected
+
+
+"""
+--- Part Two ---
+You're worried you might not ever get your items back. So worried, in fact, that your relief that a monkey's inspection didn't damage an item no longer causes your worry level to be divided by three.
+
+Unfortunately, that relief was all that was keeping your worry levels from reaching ridiculous levels. You'll need to find another way to keep your worry levels manageable.
+
+At this rate, you might be putting up with these monkeys for a very long time - possibly 10000 rounds!
+
+With these new rules, you can still figure out the monkey business after 10000 rounds. Using the same example above:
+
+== After round 1 ==
+Monkey 0 inspected items 2 times.
+Monkey 1 inspected items 4 times.
+Monkey 2 inspected items 3 times.
+Monkey 3 inspected items 6 times.
+
+== After round 20 ==
+Monkey 0 inspected items 99 times.
+Monkey 1 inspected items 97 times.
+Monkey 2 inspected items 8 times.
+Monkey 3 inspected items 103 times.
+
+== After round 1000 ==
+Monkey 0 inspected items 5204 times.
+Monkey 1 inspected items 4792 times.
+Monkey 2 inspected items 199 times.
+Monkey 3 inspected items 5192 times.
+
+== After round 2000 ==
+Monkey 0 inspected items 10419 times.
+Monkey 1 inspected items 9577 times.
+Monkey 2 inspected items 392 times.
+Monkey 3 inspected items 10391 times.
+
+== After round 3000 ==
+Monkey 0 inspected items 15638 times.
+Monkey 1 inspected items 14358 times.
+Monkey 2 inspected items 587 times.
+Monkey 3 inspected items 15593 times.
+
+== After round 4000 ==
+Monkey 0 inspected items 20858 times.
+Monkey 1 inspected items 19138 times.
+Monkey 2 inspected items 780 times.
+Monkey 3 inspected items 20797 times.
+
+== After round 5000 ==
+Monkey 0 inspected items 26075 times.
+Monkey 1 inspected items 23921 times.
+Monkey 2 inspected items 974 times.
+Monkey 3 inspected items 26000 times.
+
+== After round 6000 ==
+Monkey 0 inspected items 31294 times.
+Monkey 1 inspected items 28702 times.
+Monkey 2 inspected items 1165 times.
+Monkey 3 inspected items 31204 times.
+
+== After round 7000 ==
+Monkey 0 inspected items 36508 times.
+Monkey 1 inspected items 33488 times.
+Monkey 2 inspected items 1360 times.
+Monkey 3 inspected items 36400 times.
+
+== After round 8000 ==
+Monkey 0 inspected items 41728 times.
+Monkey 1 inspected items 38268 times.
+Monkey 2 inspected items 1553 times.
+Monkey 3 inspected items 41606 times.
+
+== After round 9000 ==
+Monkey 0 inspected items 46945 times.
+Monkey 1 inspected items 43051 times.
+Monkey 2 inspected items 1746 times.
+Monkey 3 inspected items 46807 times.
+
+== After round 10000 ==
+Monkey 0 inspected items 52166 times.
+Monkey 1 inspected items 47830 times.
+Monkey 2 inspected items 1938 times.
+Monkey 3 inspected items 52013 times.
+After 10000 rounds, the two most active monkeys inspected items 52166 and 52013 times. Multiplying these together, the level of monkey business in this situation is now 2713310158.
+
+Worry levels are no longer divided by three after each item is inspected; you'll need to find another way to keep your worry levels manageable. Starting again from the initial state in your puzzle input, what is the level of monkey business after 10000 rounds?
+"""
+
+
+@challenge(day=11)
+def over_nine_thousand_rounds(path: Path) -> int:
+    monkeys = load(path)
+    for monkey in monkeys.values():
+        monkey.worry_divisor = 1
+    for _ in trange(10000, desc="simulating", unit="rounds", leave=False):
+        for i in trange(len(monkeys), desc="running round", unit="monkeys", leave=False, delay=2.0):
             monkeys[i].take_turn(monkeys)
     top_two = heapq.nlargest(2, monkeys.values(), key=lambda m: m.num_items_inspected)
     return top_two[0].num_items_inspected * top_two[1].num_items_inspected
