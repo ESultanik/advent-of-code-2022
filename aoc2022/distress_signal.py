073cc89e2f8b7b0fe9cd609dea61cdc5fcb5a33e
Evan Sultanik <evan@sultanik.com>
2022-12-13 08:21:50 -0500

    Day 13 part 1

--- aoc2022/distress_signal.py
+++ aoc2022/distress_signal.py
@@ -109,6 +109,7 @@ What are the indices of the pairs that are already in the right order? (The firs
 Determine which pairs of packets are already in the right order. What is the sum of the indices of those pairs?
 """
 
+from functools import cmp_to_key
 from itertools import zip_longest
 import json
 from typing import Iterator, List, Optional, Tuple, Union
@@ -173,3 +174,50 @@ def in_order_pairs(path: Path) -> int:
         for i, (left, right) in enumerate(load(path))
         if compare(left, right) < 0
     )
+
+
+"""
+--- Part Two ---
+Now, you just need to put all of the packets in the right order. Disregard the blank lines in your list of received packets.
+
+The distress signal protocol also requires that you include two additional divider packets:
+
+[[2]]
+[[6]]
+Using the same rules as before, organize all packets - the ones in your list of received packets as well as the two divider packets - into the correct order.
+
+For the example above, the result of putting the packets in the correct order is:
+
+[]
+[[]]
+[[[]]]
+[1,1,3,1,1]
+[1,1,5,1,1]
+[[1],[2,3,4]]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[[1],4]
+[[2]]
+[3]
+[[4,4],4,4]
+[[4,4],4,4,4]
+[[6]]
+[7,7,7]
+[7,7,7,7]
+[[8,7,6]]
+[9]
+Afterward, locate the divider packets. To find the decoder key for this distress signal, you need to determine the indices of the two divider packets and multiply them together. (The first packet is at index 1, the second packet is at index 2, and so on.) In this example, the divider packets are 10th and 14th, and so the decoder key is 140.
+
+Organize all of the packets into the correct order. What is the decoder key for the distress signal?
+"""
+
+
+@challenge(day=13)
+def decoder_key(path: Path) -> int:
+    divider1, divider2 = [[2]], [[6]]
+    packets: List[Packet] = [divider1, divider2]
+    for p in load(path):
+        packets.extend(p)
+    packets = sorted(packets, key=cmp_to_key(compare))
+    print("\n".join(map(repr, packets)))
+    return (packets.index(divider1) + 1) * (packets.index(divider2) + 1)
